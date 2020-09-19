430f36434dc054bdef0fadd7c38a9ab749f30cbe
NiJia Lin <louis70109@gmail.com>
2020-09-19 12:39:46 +0800

    chore: update js -> ts usage

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
-const express = require("express");
+import express from "express";
+
+if (process.env.NODE_ENV != "production") require("dotenv").config();
+
+import { Request, Response } from "express/index";
 const app = express();
 const port = Number(process.env.PORT) || 5000;
 
-if (process.env.NODE_ENV != "production") {
-  require("dotenv").config();
-}
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "html");
 app.set("views", __dirname + "/views");
 
-app.get("/notify", (req: any, res: any) => {
+app.get("/notify", (req: Request, res: Response) => {
   if (req.query["liff.state"]) {
     res.render("redirect", {
       liffId: process.env.CONCAT_ID,
